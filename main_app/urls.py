ec3de19d3f8bec31b92254d14e4cee49cdf1724b
“vvela2022” <victoriavela01@gmail.com>
2022-10-07 15:16:59 -0500

    data rendering on boards page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 urlpatterns= [
     path('', views.Home.as_view(), name='home'),
-    path('about/', views.About.as_view(), name='about')
+    path('about/', views.About.as_view(), name='about'), 
+    path('boards/', views.BoardList.as_view(), name='board_list')
 ]
