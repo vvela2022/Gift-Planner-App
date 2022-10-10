ff1c0a6359734fd31df2613c52c03320c00853d3
“vvela2022” <victoriavela01@gmail.com>
2022-10-10 16:20:55 -0500

    materialize imported and some styles applied

--- main_app/views.py
+++ main_app/views.py
@@ -16,15 +16,6 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-# class Board:
-#     def __init__(self, title, image, about):
-#         self.title = title
-#         self.image = image
-#         self.about = about
-
-
-# boards = [Board("Gifts for Troy","https://cdn3.vectorstock.com/i/1000x1000/79/32/happy-birthday-gifts-pack-vector-16147932.jpg", "Gifts for my brother, Troy"), Board("Gifts for Mom", "https://media.istockphoto.com/vectors/isometric-gift-flat-icon-pixel-perfect-for-mobile-and-web-vector-id1152848595?k=20&m=1152848595&s=612x612&w=0&h=T9QQc2EYpvnB_sBzvcNrraL77VE9aXQlA86xum364uU=", "Ideas for my mom" )]
-
 class BoardList(TemplateView):
     template_name = "board_list.html"
     
