dc7585f151f1e049dbe14b3112b3fc25087fb43a
“vvela2022” <victoriavela01@gmail.com>
2022-10-10 09:16:20 -0500

    model 1 setup and rendering data in template

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.TextField(max_length=500)
+    about = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 
@@ -11,19 +12,19 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class Board:
-    def __init__(self, title, image, about):
-        self.title = title
-        self.image = image
-        self.about = about
+# class Board:
+#     def __init__(self, title, image, about):
+#         self.title = title
+#         self.image = image
+#         self.about = about
 
 
-boards = [Board("Gifts for Troy","https://cdn3.vectorstock.com/i/1000x1000/79/32/happy-birthday-gifts-pack-vector-16147932.jpg", "Gifts for my brother, Troy"), Board("Gifts for Mom", "https://media.istockphoto.com/vectors/isometric-gift-flat-icon-pixel-perfect-for-mobile-and-web-vector-id1152848595?k=20&m=1152848595&s=612x612&w=0&h=T9QQc2EYpvnB_sBzvcNrraL77VE9aXQlA86xum364uU=", "Ideas for my mom" )]
+# boards = [Board("Gifts for Troy","https://cdn3.vectorstock.com/i/1000x1000/79/32/happy-birthday-gifts-pack-vector-16147932.jpg", "Gifts for my brother, Troy"), Board("Gifts for Mom", "https://media.istockphoto.com/vectors/isometric-gift-flat-icon-pixel-perfect-for-mobile-and-web-vector-id1152848595?k=20&m=1152848595&s=612x612&w=0&h=T9QQc2EYpvnB_sBzvcNrraL77VE9aXQlA86xum364uU=", "Ideas for my mom" )]
 
 class BoardList(TemplateView):
     template_name = "board_list.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["boards"] = boards
+        context["boards"] = Board.objects.all()
         return context
